2fce42a4ef2e7f67f8094a6b715c7a5c63646409
sonsurim <surim014@naver.com>
2021-12-02 02:06:22 +0900

    ✨ feat: IconButton Component storybook구현

--- src/stories/IconButton.stories.jsx
+++ src/stories/IconButton.stories.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import IconButton from '../components/templates/IconButton'
 
 export default {
   title: 'Component/IconButton',
   component: IconButton,
-  argTypes: {},
+  argTypes: {
+    style: {
+      defaultValue: {
+        width: '30px',
+        height: '30px',
+      },
+      control: { type: 'object' },
+    },
+    className: { control: { type: 'text' } },
+    src: {
+      type: { name: 'string', require: true },
+      control: { type: 'text' },
+    },
+    alt: {
+      type: { name: 'string', require: true },
+      control: { type: 'text' },
+    },
+  },
 }
-export const Default = args => {
-  return <IconButton {...args} />
+
+const Template = args => <IconButton {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  src: '/favicon.ico',
+  alt: 'favicon',
+  onClick: action('클릭 이벤트 발생!'),
 }
