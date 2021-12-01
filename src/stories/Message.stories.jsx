be55685f996df0dc68bb0564e7d20bc44bbd4718
jooyoung cho <jzo09@naver.com>
2021-12-02 03:14:14 +0900

    ✨ feat: 기본구조 생성

---
+++ src/stories/Message.stories.jsx
@@ -0,0 +1,11 @@
+import Message from '../components/Message';
+
+export default {
+    title: 'Component/Message',
+    component: Message,
+    argTypes: {
+    },
+};
+export const Default = () => {
+    return <Message />;
+};
