1e702f4bd56f4a5ce4b7f7f090533a9b281e4d1a
sonsurim <surim014@naver.com>
2021-12-02 03:41:49 +0900

    ✨ feat: Avatar Component 기본 구성

---
+++ src/stories/components/Avatar.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Avatar from '@components/templates/Avatar'
+
+export default {
+  title: 'Component/Avatar',
+  component: Avatar,
+  argTypes: {},
+}
+const Template = args => <Avatar {...args} />
+export const Default = Template.bind({})
+Default.args = {}
