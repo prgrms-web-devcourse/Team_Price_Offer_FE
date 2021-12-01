60b31defe1a8da0d53224ea109d48cfa04b17843
jooyoung cho <jzo09@naver.com>
2021-12-02 02:57:26 +0900

    ✨ feat: 이미지 컴포넌트 구현(lazy, 404예외처리)

---
+++ src/components/templates/Image/index.jsx
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types'
+import { useState, useRef, useEffect } from 'react'
+
+let observer = null
+const LOAD_IMG_EVENT_TYPE = 'loadImage'
+
+const onIntersection = (entries, io) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      io.unobserve(entry.target)
+      entry.target.dispatchEvent(new CustomEvent(LOAD_IMG_EVENT_TYPE))
+    }
+  })
+}
+
+const Image = ({
+  lazy,
+  threshold = 0.5,
+  placeholder,
+  src,
+  width,
+  height,
+  alt,
+  mode,
+  ...props
+}) => {
+  const [loaded, setLoaded] = useState(false)
+  const imgRef = useRef(null)
+
+  const imageStyle = {
+    width,
+    height,
+    objectFit: mode, // cover, fill, contain
+  }
+
+  useEffect(() => {
+    if (!lazy) {
+      setLoaded(true)
+      return
+    }
+
+    const handleLoadImage = () => setLoaded(true)
+
+    const imgElement = imgRef.current
+    imgElement &&
+      imgElement.addEventListener(LOAD_IMG_EVENT_TYPE, handleLoadImage)
+    return () => {
+      imgElement &&
+        imgElement.removeEventListener(LOAD_IMG_EVENT_TYPE, handleLoadImage)
+    }
+  }, [lazy])
+
+  useEffect(() => {
+    if (!lazy) {
+      return
+    }
+
+    observer = new IntersectionObserver(onIntersection, { threshold })
+    imgRef.current && observer.observe(imgRef.current)
+  }, [lazy, threshold])
+  const handleImgError = e => {
+    e.target.src =
+      'https://user-images.githubusercontent.com/66211721/144287708-9f8adc66-4c14-4a4c-a1b1-dc995daa94d1.png'
+  }
+
+  return (
+    <img
+      onError={handleImgError}
+      ref={imgRef}
+      src={loaded ? src : placeholder}
+      alt={alt}
+      style={{ ...props.style, ...imageStyle }}
+    />
+  )
+}
+
+Image.propTypes = {
+  lazy: PropTypes.bool,
+  threshold: PropTypes.number,
+  src: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  alt: PropTypes.string,
+  mode: PropTypes.string,
+}
+
+export default Image
---
+++ src/stories/Image.stories.jsx
@@ -0,0 +1,63 @@
+import Image from '../components/templates/Image'
+
+export default {
+  title: 'Component/Image',
+  component: Image,
+  argTypes: {
+    lazy: {
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    src: {
+      type: { name: 'string', require: true },
+      defaultValue: 'https://picsum.photos/200',
+      control: { type: 'text' },
+    },
+    placeholder: {
+      type: { name: 'string' },
+      defaultValue: 'https://via.placeholder.com/200',
+      control: { type: 'text' },
+    },
+    threshold: {
+      type: { name: 'number' },
+      defaultValue: 0.5,
+      control: { type: 'number' },
+    },
+    width: {
+      defaultValue: 200,
+      control: { type: 'range', min: 200, max: 600 },
+    },
+    height: {
+      defaultValue: 200,
+      control: { type: 'range', min: 200, max: 600 },
+    },
+    alt: {
+      control: 'string',
+    },
+    mode: {
+      defaultValue: 'cover',
+      options: ['cover', 'fill', 'contain'],
+      control: { type: 'inline-radio' },
+    },
+  },
+}
+
+export const Default = args => {
+  return <Image {...args} />
+}
+
+export const Lazy = args => {
+  return (
+    <div>
+      {Array.from(new Array(20), (_, k) => k).map(i => (
+        <Image
+          {...args}
+          lazy
+          block
+          src={`${'https://picsum.photos/200.src'}?${i}`}
+          key={i}
+        />
+      ))}
+    </div>
+  )
+}
