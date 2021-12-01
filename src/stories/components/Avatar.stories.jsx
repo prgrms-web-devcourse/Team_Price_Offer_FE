081395f3bcba076b9626ef3dffb2877156857aac
sonsurim <surim014@naver.com>
2021-12-02 03:48:04 +0900

    ✨ feat: Avatar Component storybook 구현

--- src/stories/components/Avatar.stories.jsx
+++ src/stories/components/Avatar.stories.jsx
@@ -4,8 +4,29 @@ import Avatar from '@components/templates/Avatar'
 export default {
   title: 'Component/Avatar',
   component: Avatar,
-  argTypes: {},
+  argTypes: {
+    style: {
+      defaultValue: {
+        width: '80px',
+        height: '80px',
+      },
+      control: { type: 'object' },
+    },
+    className: { control: { type: 'text' } },
+    src: {
+      type: { name: 'string', require: true },
+      control: { type: 'text' },
+    },
+    alt: {
+      type: { name: 'string', require: true },
+      control: { type: 'text' },
+    },
+  },
 }
 const Template = args => <Avatar {...args} />
+
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  src: 'https://picsum.photos/200',
+  alt: 'avatar',
+}
