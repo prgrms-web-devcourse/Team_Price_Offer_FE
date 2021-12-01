85b6f2339238319a89edabcb68e57f0679d90966
sonsurim <surim014@naver.com>
2021-12-02 02:06:13 +0900

    ✨ feat: IconButton Component 구현

--- src/components/templates/IconButton/index.jsx
+++ src/components/templates/IconButton/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
 
-const IconButton = props => {
-  return <div />
+const IconButton = ({ style, className, alt, src, onClick }) => {
+  const handleClick = e => {
+    e.preventDefault()
+    onClick()
+  }
+
+  return (
+    <IconWrapper style={style} className={className} onClick={handleClick}>
+      <img src={src} alt={alt} />
+    </IconWrapper>
+  )
 }
 
-IconButton.propTypes = {}
+IconButton.propTypes = {
+  style: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+const IconWrapper = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+`
 
 export default IconButton
