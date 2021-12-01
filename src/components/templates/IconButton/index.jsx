e3df0950b119ff0476eddb7ef94287ecc6819baf
sonsurim <surim014@naver.com>
2021-12-02 00:34:05 +0900

    ✨ feat: IconButton Component 기본 구성

---
+++ src/components/templates/IconButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const IconButton = props => {
+  return <div />
+}
+
+IconButton.propTypes = {}
+
+export default IconButton
---
+++ src/stories/IconButton.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import IconButton from '../components/templates/IconButton'
+
+export default {
+  title: 'Component/IconButton',
+  component: IconButton,
+  argTypes: {},
+}
+export const Default = args => {
+  return <IconButton {...args} />
+}
