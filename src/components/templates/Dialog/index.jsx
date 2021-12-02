f3fb8540eb89e1bb780b46a7ca58dd2155733206
sonsurim <surim014@naver.com>
2021-12-02 18:22:30 +0900

    ✨ feat: Dialog Component 구현

--- src/components/templates/Dialog/index.jsx
+++ src/components/templates/Dialog/index.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
 
-const Dialog = props => {
-  return <div />
+const Dialog = ({
+  style,
+  className,
+  visible = false,
+  items,
+  trigger,
+  onClose,
+}) => {
+  const dialogRef = useRef(null)
+  const handleClickItem = name => {
+    console.log(`${name}으로 router 이동 처리`)
+  }
+
+  const handleClickAway = ({ target }) => {
+    if (trigger.current.contains(target)) {
+      return
+    }
+
+    !dialogRef?.current?.contains(target) && onClose()
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', e => handleClickAway(e))
+
+    return () => {
+      document.removeEventListener('click', e => handleClickAway(e))
+    }
+  }, [])
+
+  return (
+    <DialogWrapper
+      ref={dialogRef}
+      visible={visible}
+      style={style}
+      className={className}>
+      {items.map(item => (
+        <li key={`test-${item.name}`}>
+          <span onClick={() => handleClickItem(item.name)}>{item.text}</span>
+        </li>
+      ))}
+    </DialogWrapper>
+  )
 }
 
-Dialog.propTypes = {}
+Dialog.propTypes = {
+  style: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+  visible: PropTypes.bool,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  trigger: PropTypes.shape({}).isRequired,
+  onClose: PropTypes.func,
+}
+
+const DialogWrapper = styled.ul(({ visible }) => ({
+  display: visible ? 'block' : 'none',
+  position: 'absolute',
+  width: '147px',
+  margin: 0,
+  padding: '20px',
+  listStyle: 'none',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  backgroundColor: '#fff',
+
+  li: {
+    '&:not(:first-of-type, :last-child)': {
+      padding: '20px 0',
+    },
+
+    span: {
+      cursor: 'pointer',
+    },
+  },
+}))
 
 export default Dialog
