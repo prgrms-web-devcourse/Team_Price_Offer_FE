dca3ffb46883ae4f67553d3db5aa4b752eb8cfa2
sonsurim <surim014@naver.com>
2021-12-02 18:22:37 +0900

    ✨ feat: Dialog Component storybook 구현

--- src/stories/components/Dialog.stories.jsx
+++ src/stories/components/Dialog.stories.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Dialog from '@components/templates/Dialog'
 
 export default {
   title: 'Component/Dialog',
   component: Dialog,
-  argTypes: {},
+  argTypes: {
+    style: {
+      control: { type: 'object' },
+    },
+    className: { control: { type: 'text' } },
+    items: {
+      defaultValue: [
+        {
+          name: 'profile',
+          text: '내 프로필',
+        },
+        {
+          name: 'message',
+          text: '내 쪽지함',
+        },
+        {
+          name: 'logout',
+          text: '로그아웃',
+        },
+      ],
+      control: { type: 'array' },
+    },
+  },
 }
-const Template = args => <Dialog {...args} />
-export const Default = Template.bind({})
-Default.args = {}
+
+export const Default = () => {
+  const [visible, setVisible] = useState(false)
+  const buttonRef = useRef(null)
+
+  const items = [
+    {
+      name: 'profile',
+      text: '내 프로필',
+    },
+    {
+      name: 'message',
+      text: '내 쪽지함',
+    },
+    {
+      name: 'logout',
+      text: '로그아웃',
+    },
+  ]
+
+  return (
+    <>
+      <button ref={buttonRef} onClick={() => setVisible(true)}>
+        Show
+      </button>
+      <Dialog
+        trigger={buttonRef}
+        items={items}
+        visible={visible}
+        onClose={() => setVisible(false)}
+      />
+    </>
+  )
+}
