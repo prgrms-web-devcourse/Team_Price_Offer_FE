05b1e8dbd27aef2e0f68c9eb44f7f31fe18f9f0b
jooyoung cho <jzo09@naver.com>
2021-12-03 14:35:40 +0900

    ♻️ refactor: 리뷰반영 && radius 수정

--- src/components/templates/Message/index.jsx
+++ src/components/templates/Message/index.jsx
@@ -1,54 +1,30 @@
-import React from 'react'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
-import { ustState,useEffect } from 'react'
 
 const Message = ({ style, className, children }) => {
-    // const onChange = e => {
-    //     console.log(e.target.value)
-    // }
-    const checkMessageLength = () => {
-        // const [messageHeight , setHeight] = useState(40)
-        const messageLength = children.length
-        const messageHeight = (messageLength / 12) * 40
-        style.height = `${40 + messageHeight}px`
-    }
-    useEffect(() => {
-        checkMessageLength()
-    })
-    return (
-        <MessageBox style={style} className={className}>
-            {children}
-        </MessageBox>
-    )
-    
+  return (
+    <MessageBox style={style} className={className}>
+      {children}
+    </MessageBox>
+  )
 }
-
-
 
 Message.propTypes = {
-    style: PropTypes.objectOf(PropTypes.string),
-    className: PropTypes.string,
-    children: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+  children: PropTypes.string,
 }
 
-
-
 const MessageBox = styled.div`
-    width: 180px;
-    height: 100%;
-    color:#000000;
-    font-size:14px;
-    background-color:#EEEEEE;
-    padding-top:10px;
-    padding-bottom:10px;
-    padding-left:19px;
-    padding-right:19px;
-    border:none;
-    border-radius:30;
-
-    
+  max-width: 180px;
+  height: 100%;
+  word-break: break-all;
+  color: #000000;
+  font-size: 14px;
+  background-color: #eeeeee;
+  padding: 10px 19px;
+  border: none;
+  border-radius: 30px;
 `
-
 
 export default MessageBox
--- src/stories/components/Message.stories.jsx
+++ src/stories/components/Message.stories.jsx
@@ -1,33 +1,33 @@
-import Message from '../../components/templates/Message';
+import Message from '../../components/templates/Message'
 
 export default {
-    title: 'Component/Message',
-    component: Message,
-    argTypes: {
-        style: {
-            defaultValue: {
-                width: '180px',
-                height: '100%',
-                color:'#000000',
-                fontSize:'14px',
-                backgroundColor:'#EEEEEE',
-                paddingLeft:'19px',
-                paddingRight:'19px',
-                border:'none',
-                borderRadius:'30',
-            },
-            control: { type: 'object' },
-        },
-
-        className: {
-            control:'text'
-        },
-        
-        children: {
-            control:'text'
-        }
+  title: 'Component/Message',
+  component: Message,
+  argTypes: {
+    style: {
+      defaultValue: {
+        width: '180px',
+        height: '100%',
+        color: '#000000',
+        fontSize: '14px',
+        backgroundColor: '#EEEEEE',
+        paddingLeft: '19px',
+        paddingRight: '19px',
+        border: 'none',
+        borderRadius: '30',
+      },
+      control: { type: 'object' },
     },
-};
-export const Default = (args) => {
-    return <Message {...args} />;
-};
+
+    className: {
+      control: 'text',
+    },
+
+    children: {
+      control: 'text',
+    },
+  },
+}
+export const Default = args => {
+  return <Message {...args} />
+}
