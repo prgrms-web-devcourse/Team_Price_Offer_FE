1dc534f330fab0009a01b0f3c53129bdc0185f22
jooyoung cho <jzo09@naver.com>
2021-12-03 11:55:36 +0900

    ♻️ refactor: 린트적용 && value 없애기

--- src/components/templates/Textarea/index.jsx
+++ src/components/templates/Textarea/index.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 
-const textArea = ({style, className, onChange, value, name , placeholder})=> {
-    return (
-        <TextArea placeholder={placeholder} style={style} className={className} onChange={onChange} name={name} >
-        </TextArea>
-    )
+const TextArea = ({ style, className, onChange, name, placeholder }) => {
+  return (
+    <ComonTextArea
+      placeholder={placeholder}
+      style={style}
+      className={className}
+      onChange={onChange}
+      name={name}
+    />
+  )
 }
 
-textArea.propTypes = {
-    style: PropTypes.objectOf(PropTypes.string),
-    className: PropTypes.string,
-    onChange: PropTypes.func,
+TextArea.propTypes = {
+  style: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+  onChange: PropTypes.func,
 }
 
+const ComonTextArea = styled.textarea`
+  width: 324px;
+  height: 230px;
+  padding: 20px;
 
-const TextArea = styled.textarea`
-    width: 324px;
-    height: 230px;
-    padding:20px;
-
-    &::placeholder {
-        font-size:14px;
-        color: #888888
-    }
+  &::placeholder {
+    font-size: 14px;
+    color: #888888;
+  }
 `
-export default textArea
+export default TextArea
