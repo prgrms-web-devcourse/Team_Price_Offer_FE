768a1c55d7c888c2ed3dd0853980a277b836182f
jooyoung cho <jzo09@naver.com>
2021-12-06 23:57:04 +0900

    💄 design: 반응형 1차 추가 및 svg파일 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,61 +2,60 @@ import Button from '@components/templates/Button'
 import IMG from '@components/templates/Image'
 import ICONBUTTON from '@components/templates/IconButton'
 import DIVIDER from '@components/templates/Divider'
+import Like from '@components/templates/ToggleButton'
 import styles from '@assets/css/Main.module.css'
-import { ReactComponent as Bestgoods } from '../assets/svg/category_bestgoods.svg'
 
 const MainPage = () => {
   const categoryList = [
     {
       value: 1,
       text: '인기상품',
-      // src: require('../assets/svg/category_bestgoods.svg'),
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_bestgoods.svg').default.src,
     },
     {
       value: 2,
       text: '여성의류',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_womenwear.svg').default.src,
     },
     {
       value: 3,
       text: '여성잡화',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_womengoods.svg').default.src,
     },
     {
       value: 4,
       text: '생활 가전',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_lifegoods.svg').default.src,
     },
     {
       value: 5,
       text: '가구/인테리어',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_furniture.svg').default.src,
     },
     {
       value: 6,
       text: '게임/취미',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_game.svg').default.src,
     },
     {
       value: 7,
       text: '도서',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_book.svg').default.src,
     },
     {
       value: 8,
       text: '스포츠/레저',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_sports.svg').default.src,
     },
     {
       value: 9,
       text: '반려동물 제품',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_petgoods.svg').default.src,
     },
     {
       value: 10,
       text: '나눔',
-      src: '/favicon.ico',
+      src: require('../assets/images/icon/category_share.svg').default.src,
     },
   ]
   const goodsList = [
@@ -116,31 +115,31 @@ const MainPage = () => {
       address: '서울시 강북구',
       time: '2분전',
       price: '160',
-      },
-      {
-        id: 8,
-        src: '',
-        title: '급처급처',
-        address: '서울시 강북구',
-        time: '2분전',
-        price: '160',
-      },
-      {
-        id: 9,
-        src: '',
-        title: '급처급처',
-        address: '서울시 강북구',
-        time: '2분전',
-        price: '160',
-      },
-      {
-        id: 10,
-        src: '',
-        title: '급처급처',
-        address: '서울시 강북구',
-        time: '2분전',
-        price: '160',
-      },
+    },
+    {
+      id: 8,
+      src: '',
+      title: '급처급처',
+      address: '서울시 강북구',
+      time: '2분전',
+      price: '160',
+    },
+    {
+      id: 9,
+      src: '',
+      title: '급처급처',
+      address: '서울시 강북구',
+      time: '2분전',
+      price: '160',
+    },
+    {
+      id: 10,
+      src: '',
+      title: '급처급처',
+      address: '서울시 강북구',
+      time: '2분전',
+      price: '160',
+    },
   ]
   const categoryClick = () => {
     // alert(`${categoryList.text} 선택`)
@@ -150,22 +149,16 @@ const MainPage = () => {
   return (
     <>
       <div className={styles.headerWrapper}>
-        <div
-          className={styles.header}/>
+        <div className={styles.header} />
       </div>
-      <div
-        className={styles.bannerWrapper}>
-        <div className={styles.banner}>
-          banner
-        </div>
+      <div className={styles.bannerWrapper}>
+        <div className={styles.banner}>banner</div>
       </div>
 
       <div className={styles.categoryWrapper}>
         <div className={styles.categoryList}>
           {categoryList.map(categoryList => (
-              <div
-              className={styles.category}
-              key={categoryList.value}>
+            <div className={styles.category} key={categoryList.value}>
               <ICONBUTTON
                 src={categoryList.src}
                 alt={categoryList.text}
@@ -176,63 +169,64 @@ const MainPage = () => {
                 }}
                 //   onClick={categoryClick(categoryList)}
               />
-              <div className={styles.categorytext}>
-                {categoryList.text}
-              </div>
+              <div className={styles.categorytext}>{categoryList.text}</div>
             </div>
           ))}
         </div>
       </div>
-      
-    <div className={ styles.newgoodstextWrapper}>
-              <div className={styles.newgoodstextContainer}>
-                  <div
-                      className={styles.newgoodsTitle}>신규상품
-                  </div>
-              </div>
-    </div>
-    
-     <div className={styles.newgoodsWwrapper}>
+
+      <div className={styles.newgoodstextWrapper}>
+        <div className={styles.newgoodstextContainer}>
+          <div className={styles.newgoodsTitle}>신규상품</div>
+        </div>
+      </div>
+
+      <div className={styles.newgoodsWwrapper}>
         <ul className={styles.newgoodsList}>
           {goodsList.map(goodsList => (
-              <li
-                  className={styles.newgoods}
-                  key={goodsList.id}>
+            <li className={styles.newgoods} key={goodsList.id}>
               <IMG
-                src="/favicon.ico"
+                src="https://picsum.photos/200"
                 width="183px"
                 height="227px"
+                ration="rectangle-h"
                 style={{
                   border: 'none',
                 }}
               />
-              <ICONBUTTON
-                src={categoryList.src}
-                alt={categoryList.text}
+              <div
+                className="likeBorder"
                 style={{
                   position: 'absolute',
                   top: '15px',
                   right: '10px',
-                  // left: '143px',
-                  width: '40px',
-                  height: '40px',
-                  alignSelf: 'center',
-                }}
-                //   onClick={categoryClick(categoryList)}
-              />
+                  width: '30px',
+                  height: '30px',
+                  backgroundColor: '#FFFFFF',
+                  borderRadius: '50px',
+                }}>
+                <Like
+                  src={categoryList.src}
+                  alt={categoryList.text}
+                  style={{
+                    position: 'absolute',
+                    top: '5.3px',
+                    right: '5.1px',
+                    width: '20px',
+                    height: '20px',
+                    alignSelf: 'center',
+                  }}
+                  //   onClick={categoryClick(categoryList)}
+                />
+              </div>
               <div>
-                      <div className={ styles.infoTop}>
-                  {goodsList.title}
-                </div>
+                <div className={styles.infoTop}>{goodsList.title}</div>
                 <div className={styles.infoMiddle}>
                   {goodsList.address}
                   <DIVIDER type="vertical" style={{ color: '#DDDDDD' }} />
                   {goodsList.time}
                 </div>
-                <div
-                  className={styles.infoBottom}>
-                  {goodsList.price} 원
-                </div>
+                <div className={styles.infoBottom}>{goodsList.price} 원</div>
               </div>
             </li>
           ))}
