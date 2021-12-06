d1511888a6684fa744ca6a49e780c222fb5e6770
Shinhyojeong <hyojeonge0@naver.com>
2021-12-06 15:02:29 +0900

    🔧 config : sass 추가

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    reactStrictMode: true,
+  },
 }
