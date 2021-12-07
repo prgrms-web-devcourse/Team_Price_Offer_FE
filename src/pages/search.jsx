ec410be3ed2cb17c7ab5e04cfe9bbf7ba96a726c
sonsurim <surim014@naver.com>
2021-12-07 14:56:03 +0900

    ♻️ refactor: search page css명 수정

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -36,28 +36,28 @@ const search = props => {
   }
 
   return (
-    <>
-      <div className={isopenedFilter ? 'filter_mask show' : 'filter_mask'}>
-        <div className="filter_container" ref={ref}>
-          <div className="filter_wrapper">
-            <div className="filter-cont_wrapper">
+    <div className="search">
+      <div className={isopenedFilter ? 'filter-mask show' : 'filter-mask'}>
+        <div className="filter-container" ref={ref}>
+          <div className="filter-wrapper">
+            <div className="filter-cont-wrapper">
               <div className="filter-cont">
-                <h3 className="cont-title">카테고리</h3>
-                <ul className="cont-item_list category">
+                <h3 className="filter-cont_title">카테고리</h3>
+                <ul className="filter-cont_item-list category">
                   {CATEGORIES.map(({ code, name }) => (
-                    <li key={code} className="cont-item">
+                    <li key={code} className="filter-cont_item">
                       <span>{name}</span>
                     </li>
                   ))}
                 </ul>
               </div>
               <div className="filter-cont">
-                <h3 className="cont-title">거래방식</h3>
-                <div className="cont-item_list orderway">
+                <h3 className="filter-cont_title">거래방식</h3>
+                <div className="filter-cont_item-list orderway">
                   <Radio
                     formName="orderWay"
                     items={ORDERWAY}
-                    className="cont-item"
+                    className="filter-cont_item"
                     size="21px"
                     fontSize="14px"
                     radioDirection="vertical"
@@ -65,42 +65,42 @@ const search = props => {
                 </div>
               </div>
               <div className="filter-cont">
-                <h3 className="cont-title">가격</h3>
-                <div className="cont-item_list">
-                  <p className="cont-item_input">
+                <h3 className="filter-cont_title">가격</h3>
+                <div className="filter-cont_item-list">
+                  <p className="filter-cont_item-input">
                     <Input
                       placeholder="최소 가격"
                       style={{ ...inputStyle, marginRight: '20px' }}
                     />
                     <Input placeholder="최대 가격" style={inputStyle} />
                   </p>
-                  <p className="cont-item_notice">
+                  <p className="filter-cont_item-notice">
                     가격은 숫자로만 입력할 수 있어요!
                   </p>
                 </div>
               </div>
             </div>
-            <div className="filter-btn_wrapper">
+            <div className="result-filter_btn-wrapper">
               <Button style={{ ...btnStyle, ...resetBtnStyle }}>초기화</Button>
               <Button style={btnStyle}>필터 적용</Button>
             </div>
           </div>
         </div>
       </div>
-      <div className="search-result_container">
-        <div className="search-result_header">
-          <div className="search-result_title">
+      <div className="result-container">
+        <div className="result-header">
+          <div className="result-title">
             <h3>"maison margiela"의 검색결과</h3>
             <span>30</span>
           </div>
-          <div className="search-result_filter-wrapper">
-            <div className="search-result_filter">
+          <div className="result-filter-wrapper">
+            <div className="result-filter">
               <div
-                className="filter-btn"
+                className="result-filter_btn"
                 onClick={() => setIsopenedFilter(!isopenedFilter)}>
                 <span>필터</span>
                 <IconButton
-                  className="btn-icon"
+                  className="result-filter_btn-icon"
                   src={require('@assets/images/icon/filter.svg').default.src}
                   alt="필터"
                   onClick={() => {
@@ -109,46 +109,56 @@ const search = props => {
                 />
               </div>
             </div>
-            <div className="search-result_filter-sort">
-              <div className="filter-sort recent active">최신순</div>
-              <div className="filter-sort price-low">낮은 가격순</div>
-              <div className="filter-sort price-high">높은 가격순</div>
+            <div className="result-filter_sort">
+              <div className="result-filter_sort-item recent active">
+                최신순
+              </div>
+              <div className="result-filter_sort-item price-low">
+                낮은 가격순
+              </div>
+              <div className="result-filter_sort-item price-high">
+                높은 가격순
+              </div>
             </div>
           </div>
         </div>
-        <div className="search-result_body">
-          <div className="search-result_item">
-            <div className="item-cont">
-              <Image
-                className="cont-img"
-                width="180px"
-                height="225px"
-                src="https://picsum.photos/200/300"
-              />
-              <IconButton
-                className="cont-heart"
-                src={require('@assets/images/icon/heart_blank.svg').default.src}
-                alt="필터"
-                onClick={() => {
-                  console.log('필터')
-                }}
-              />
+        <div className="result-body">
+          <div className="result-item-list">
+            <div className="result-item">
+              <div className="result-item_cont">
+                <Image
+                  className="result-item_cont-img"
+                  width="180px"
+                  height="225px"
+                  src="https://picsum.photos/200/300"
+                />
+                <IconButton
+                  className="result-item_cont-heart"
+                  src={
+                    require('@assets/images/icon/heart_blank.svg').default.src
+                  }
+                  alt="필터"
+                  onClick={() => {
+                    console.log('필터')
+                  }}
+                />
+              </div>
+              <p className="result-item_title">MaisonMargiela wool swater</p>
+              <div className="result-item_meta">
+                <span>관악구 봉천동</span>
+                <Divider
+                  type="vertical"
+                  height="8"
+                  style={{ border: '1px solid #ddd' }}
+                />
+                <span>2분 전</span>
+              </div>
+              <p className="result-item_price">165,000원</p>
             </div>
-            <p className="item-title">MaisonMargiela wool swater</p>
-            <div className="item-meta">
-              <span>관악구 봉천동</span>
-              <Divider
-                type="vertical"
-                height="8"
-                style={{ border: '1px solid #ddd' }}
-              />
-              <span>2분 전</span>
-            </div>
-            <p className="item-price">165,000원</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
