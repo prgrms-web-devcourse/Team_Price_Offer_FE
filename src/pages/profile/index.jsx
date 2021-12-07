c055c6a9408dbb1877578b272bc6b5bec91527b5
Shinhyojeong <hyojeonge0@naver.com>
2021-12-08 03:18:34 +0900

    ♻️ refactor : Profile Page className 변경

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -11,11 +11,6 @@ const profile = () => {
     objectFit: 'cover',
   }
 
-  const imgStyle = {
-    maxWidth: '180px',
-    maxHeight: '230px',
-  }
-
   const goodsList = [
     {
       id: 1,
@@ -101,54 +96,54 @@ const profile = () => {
   ]
 
   return (
-    <div className="profile_wrapper">
-      <div className="profile_container">
-        <div className="profile_box">
+    <div className="profile">
+      <div className="profile-container">
+        <div className="profile-box">
           <Avatar
-            className="profile_img"
+            className="profile-box_img"
             style={profileImgStyle}
             src="https://w.namu.la/s/69388e6fe9921a1ed22ef19263516ab891b1cc90862c126cde56f200b29a84e3f3d9e56055f0d09c3d42b44ad7d2b0b19194150da1dc6fae31efb66dd4b85d7047660f1da1f6a0d73ecbe134e12e8ba9"
           />
-          <div className="profile_box_inform">
-            <span className="profile_nickname">히텧</span>
-            <span className="profile_level">Level 1</span>
-            <span className="profile_area">동작구 사당동</span>
+          <div className="profile-box_inform">
+            <span className="profile-box_nickname">히텧</span>
+            <span className="profile-box_level">Level 1</span>
+            <span className="profile-box_area">동작구 사당동</span>
           </div>
         </div>
         <Divider type="vertical" style={{ height: '100%' }} />
 
-        <div className="profile_list_box">
-          <div className="profile_list_item">
-            <div className="profile_list_title">판매 상품</div>
-            <div className="profile_list_content">30</div>
-          </div>
-          <div className="profile_list_item">
-            <div className="profile_list_title">찜한 상품</div>
-            <div className="profile_list_content">60</div>
-          </div>
-          <div className="profile_list_item">
-            <div className="profile_list_title">가격 제안</div>
-            <div className="profile_list_content">30</div>
-          </div>
-          <div className="profile_list_item">
-            <div className="profile_list_title">거래 후기</div>
-            <div className="profile_list_content">30</div>
-          </div>
-        </div>
+        <ul className="profile-list">
+          <li className="profile-list_item">
+            <div className="profile-list_title">판매 상품</div>
+            <div className="profile-list_content">30</div>
+          </li>
+          <li className="profile-list_item">
+            <div className="profile-list_title">찜한 상품</div>
+            <div className="profile-list_content">60</div>
+          </li>
+          <li className="profile-list_item">
+            <div className="profile-list_title">가격 제안</div>
+            <div className="profile-list_content">30</div>
+          </li>
+          <li className="profile-list_item">
+            <div className="profile-list_title">거래 후기</div>
+            <div className="profile-list_content">30</div>
+          </li>
+        </ul>
       </div>
 
-      <div className="profile_contents">
-        <div className="contents_title">가격 제안</div>
-        <div className="tab_box">
-          <Button className="item_status_btn">판매 중</Button>
-          <Button className="item_status_btn">판매 완료</Button>
+      <div className="result-container">
+        <div className="result-title">가격 제안</div>
+        <div className="result-btn-box">
+          <Button className="result-btn_item">판매 중</Button>
+          <Button className="result-btn_item">판매 완료</Button>
         </div>
-        <div className="lineup">
-          <span className="item">최신순</span>
+        <div className="result-lineup-box">
+          <span className="result-lineup_item">최신순</span>
           <Divider type="vertical" />
-          <span className="item">낮은 가격순</span>
+          <span className="result-lineup_item">낮은 가격순</span>
           <Divider type="vertical" />
-          <span className="item">높은 가격순</span>
+          <span className="result-lineup_item">높은 가격순</span>
         </div>
         <div className="newgoods-wrapper">
           <ul className="newgoods-list">
