97b24854fb23d713d2995810f1dac380902188e4
jooyoung cho <jzo09@naver.com>
2021-12-07 14:33:46 +0900

    ✨ feat: 디렉토리 구조 변경

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import '../styles/reset.min.css'
 import '../styles/font.css'
 import '../styles/globals.css'
+import '@styles/mainpage.scss'
 // import styles from '@assets/css/Main.css'
 
 function MyApp({ Component, pageProps }) {
