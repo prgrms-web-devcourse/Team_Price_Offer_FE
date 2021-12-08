d84c7e6379977d90401f67c9f972e3109e3e07bc
Shinhyojeong <hyojeonge0@naver.com>
2021-12-09 02:25:06 +0900

    💄 design : ProductItem Component scss 적용

--- src/components/ui/ProductItem/index.jsx
+++ src/components/ui/ProductItem/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Image from '@components/templates/Image'
 import Divider from '@components/templates/Divider'
-import IconButton from '@components/templates/IconButton'
+import Like from '@components/templates/ToggleButton'
 
-const ProductItem = props => {
-  const imgStyle = {
+const ProductItem = ({ src, title, time, location, price }) => {
+  const imgContainerStyle = {
+    width: '100%',
+    height: '300px',
+    marginBottom: '10px',
     position: 'relative',
+  }
+  const imgStyle = {
     width: '100%',
     height: '100%',
   }
@@ -15,42 +20,59 @@ const ProductItem = props => {
     position: 'absolute',
     top: '15px',
     right: '10px',
-    width: '30px !important',
-    height: '30px !important',
+    width: '35px !important',
+    height: '35px !important',
+    backgroundColor: 'white',
+    opacity: '.7',
+    borderRadius: '100%',
+    padding: '6px',
+    boxSizing: 'border-box',
   }
 
   return (
     <div className="product">
-      <div className="product-img-container">
+      <div style={imgContainerStyle} className="product-img-container">
         <Image
           style={imgStyle}
           className="product-img"
-          src="https://picsum.photos/200/300"
+          src={src}
+          mode="cover"
+          ratio="rectangle-h"
+          placeholder="https://www.urbansplash.co.uk/images/placeholder-16-9.jpg"
+          alt={title}
         />
-        <IconButton
+        <Like
           style={heartIconStyle}
           className="product-icon_heart"
-          src={require('@assets/images/icon/heart_blank.svg')}
-          alt="필터"
+          src={require('@assets/images/icon/heart_blank.svg').default.src}
+          alt="좋아요"
           onClick={() => {
-            console.log('필터')
+            console.log('좌요~좌요~')
           }}
         />
       </div>
-      <p className="product-cont_title">MaisonMargiela wool swater</p>
-      <div className="product-cont_meta">
-        <span>관악구 봉천동</span>
-        <Divider
-          type="vertical"
-          height="8"
-          style={{ border: '1px solid #ddd' }}
-        />
-        <span>2분 전</span>
+      <div className="product-cont">
+        <p className="product-cont_title">{title || '상품 이름 없음'}</p>
+        <div className="product-cont_meta">
+          <span>{location || '지역 정보 없음'}</span>
+          <Divider
+            type="vertical"
+            height="8"
+            style={{ border: '1px solid #ddd' }}
+          />
+          <span>{time || '시간 정보 없음'}</span>
+        </div>
+        <p className="product-cont_price">{price || '가격 정보 없음'}</p>
       </div>
-      <p className="product-cont_price">165,000원</p>
     </div>
   )
 }
-ProductItem.propTypes = {}
+ProductItem.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  time: PropTypes.string,
+  location: PropTypes.string,
+  price: PropTypes.number,
+}
 
 export default ProductItem
