b4955c213a6ed23eb2f0c5d0baaa19e003a96769
Shinhyojeong <hyojeonge0@naver.com>
2021-12-09 10:08:14 +0900

    ✨feat : ProductItemList Component 마크업

--- src/components/ui/ProductItemList/index.jsx
+++ src/components/ui/ProductItemList/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ProductItem from '@components/ui/ProductItem'
 
-const index = props => {
-  return <div />
+const ProdductItemList = ({ goodsList, className }) => {
+  return (
+    <div className={`goods-wrapper ${className}`}>
+      <ul className="goods-list">
+        {goodsList.map(item => (
+          <ProductItem
+            key={item.id}
+            src="https://img.khan.co.kr/news/2021/08/15/l_2021081501002249400192111.webp"
+            title={item.title}
+            location={item.address}
+            time={item.time}
+            price={item.price}
+          />
+        ))}
+      </ul>
+    </div>
+  )
+}
+ProdductItemList.propTypes = {
+  goodsList: PropTypes.arrayOf(PropTypes.object).isRequired,
+  className: PropTypes.string,
 }
 
-index.propTypes = {}
-
-export default index
+export default ProdductItemList
