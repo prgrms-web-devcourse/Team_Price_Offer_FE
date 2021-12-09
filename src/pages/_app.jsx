3347fe1b830eefef5558327d54ed2fbf6963c791
sonsurim <surim014@naver.com>
2021-12-09 11:27:35 +0900

    ✨ feat: Modal Login 구현

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ import '@styles/pages/index.scss'
 import '@styles/pages/postDetail.scss'
 import '@styles/pages/posting.scss'
 import '@styles/pages/message.scss'
+import '@styles/ui/modal.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
