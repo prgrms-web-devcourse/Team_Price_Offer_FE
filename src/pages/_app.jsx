770cc4a1a486b083f897505f6704950bc38e667c
jooyoung cho <jzo09@naver.com>
2021-12-11 16:39:42 +0900

    ✨ feat: 로그인 유무 기능

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -13,14 +13,29 @@ import '@styles/pages/message.scss'
 import '@styles/ui/modal.scss'
 import '@styles/ui/modalConfirmBuyer.scss'
 import '@styles/ui/modalReview.scss'
+import { useState} from 'react'
 import Header from '@components/ui/Header'
+import ModalLogin from '@components/ui/modal/ModalLogin'
+import useStorage from '@utils/storage.js'
 
 function MyApp({ Component, pageProps }) {
+  const isBrowser = (() => typeof window !== 'undefined')();
+  let isLogin
+  const { getItem, setItem,removeItem } = useStorage();
+  if (getItem('userToken')) {
+    isLogin=true 
+  } 
+  else {
+    isLogin=false
+  }
+  console.log(isLogin)
+
   return (
     <div className="container">
       <div className="wrapper">
-        <Header isLogin={false} />
+        <Header isLogin={isLogin} />
         <Component {...pageProps} />
+        {/* <ModalLogin visible/> */}
       </div>
     </div>
   )
