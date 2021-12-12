a3e816195b799696e0f1cf7bc41ed1fbbed56b66
sonsurim <surim014@naver.com>
2021-12-13 01:27:48 +0900

    ♻️ refactor: validation 동적으로 변경

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,27 +1,37 @@
 const validate = values => {
   const errors = {}
+  const fieldKeys = Object.keys(values)
+  const haveEmail = fieldKeys.includes('email')
+  const haveNickname = fieldKeys.includes('nickname')
+  const havePassword = fieldKeys.includes('password')
+  const haveConfirmedPassword = fieldKeys.includes('confirmedPassword')
 
-  Object.keys(values).forEach(value => {
+  fieldKeys.forEach(value => {
     if (!values[value]) {
       errors[value] = '필수값을 입력해주세요!'
     }
   })
 
-  if (!/^[A-Z0-9._%+-]+@[A-Z0-s9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  if (
+    haveEmail &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-s9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  ) {
     errors.email = '이메일 형식이 아닙니다.'
   }
-  if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i.test(values.password)) {
+
+  if (
+    havePassword &&
+    !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i.test(values.password)
+  ) {
     errors.password = '비밀번호 조건을 확인해 주세요'
   }
 
-  if (!values.password && values.confirmedPassword) {
-    errors.confirmedPassword = '비밀번호를 입력해 주세요'
-  } else if (values.password && values.password !== values.confirmedPassword) {
+  if (haveConfirmedPassword && values.password !== values.confirmedPassword) {
     errors.confirmedPassword = '비밀번호가 일치하지 않습니다'
   }
 
   if (
-    values.nickname &&
+    haveNickname &&
     (values.nickname.length < 2 || values.nickname.length > 15)
   ) {
     errors.nickname = '닉네임 문자 개수를 확인해 주세요 (2~15자)'
