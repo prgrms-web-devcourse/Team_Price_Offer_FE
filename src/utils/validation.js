2d3008a859927daaa3befefb25491cc6c1cb4ff5
Shinhyojeong <hyojeonge0@naver.com>
2021-12-13 00:52:26 +0900

    ♻️ refactor : validation 코드 정리

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -10,21 +10,13 @@ const validate = values => {
   if (!/^[A-Z0-9._%+-]+@[A-Z0-s9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = '이메일 형식이 아닙니다.'
   }
-  if (
-    values.password &&
-    !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i.test(values.password)
-  ) {
-    errors.password =
-      '비밀번호 조건을 확인해 주세요 (영문, 숫자 하나 이상 포함, 8~16자)'
+  if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/i.test(values.password)) {
+    errors.password = '비밀번호 조건을 확인해 주세요'
   }
 
   if (!values.password && values.confirmedPassword) {
     errors.confirmedPassword = '비밀번호를 입력해 주세요'
-  } else if (
-    values.password &&
-    values.confirmedPassword &&
-    values.password !== values.confirmedPassword
-  ) {
+  } else if (values.password && values.password !== values.confirmedPassword) {
     errors.confirmedPassword = '비밀번호가 일치하지 않습니다'
   }
 
