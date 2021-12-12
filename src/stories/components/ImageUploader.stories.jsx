1e6500d535628d5d320842d4c0d95747166dfca2
sonsurim <surim014@naver.com>
2021-12-12 23:25:46 +0900

    ✨ feat: 이미지 업로더 스토리북 구현

--- src/stories/components/ImageUploader.stories.jsx
+++ src/stories/components/ImageUploader.stories.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ImageUploader from '@components/templates/ImageUploader'
+import { imgApi } from '@api/apis'
 
 export default {
   title: 'Component/ImageUploader',
@@ -7,7 +8,41 @@ export default {
   argTypes: {},
 }
 
-const Template = args => <ImageUploader {...args} />
+export const Default = () => {
+  const handleChange = async e => {
+    const formData = new FormData()
+    const imageFile = e.target.files[0]
+    formData.append('image', imageFile)
 
-export const Default = Template.bind({})
-Default.args = {}
+    const res = await imgApi.convertImg(formData)
+
+    if (Number(res.code) === 200) {
+      imgRef.current.src = res.data?.imageUrl
+      return
+    }
+
+    alert('API CORS 오류로 인해 임시로 이미지를 출력합니다.')
+    const reader = new FileReader()
+    reader.readAsDataURL(imageFile)
+
+    reader.onload = () => {
+      imgRef.current.src = reader.result
+    }
+  }
+
+  const imgRef = useRef(null)
+  const imgUrl = 'https://picsum.photos/200'
+  const imgStyle = {
+    width: '100px',
+    height: '100px',
+    borderRadius: '100%',
+  }
+
+  return (
+    <ImageUploader onChange={handleChange}>
+      <span>
+        <img ref={imgRef} style={imgStyle} src={imgUrl} alt="" />
+      </span>
+    </ImageUploader>
+  )
+}
