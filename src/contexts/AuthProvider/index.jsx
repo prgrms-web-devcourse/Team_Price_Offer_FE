fb482cb877368a9822b7698b3d447be30e37eee8
sonsurim <surim014@naver.com>
2021-12-13 22:58:55 +0900

    ✨ feat: AuthProvider 기본 구조 구현

---
+++ src/contexts/AuthProvider/index.jsx
@@ -0,0 +1,75 @@
+// Provider
+import React, { createContext, useReducer } from 'react'
+import PropTypes from 'prop-types'
+import Header from '@components/ui/Header'
+import Spinner from '@components/templates/Spinner'
+import AuthReducer from './reducer'
+import useActions from './actions'
+
+const initialState = {
+  isLoading: true,
+  token: null,
+  userData: {
+    id: null,
+    email: null,
+    appleLevel: null,
+    nickname: null,
+    profileImage: null,
+    address: null,
+  },
+}
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, initialState)
+
+  console.log(state)
+
+  const {
+    handleEmailLogin,
+    handleKakaoLogin,
+    handleSignup,
+    handleWithDrawal,
+    handleLogout,
+    handleGetUserInfo,
+    handleLoadingOn,
+    handleLoadingOff,
+  } = useActions(dispatch)
+
+  if (state.isLoading) {
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <Header />
+          <div className="spinner-wrapper">
+            <Spinner loading={state.isLoading} size={70} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        handleEmailLogin,
+        handleKakaoLogin,
+        handleSignup,
+        handleWithDrawal,
+        handleLogout,
+        handleGetUserInfo,
+        handleLoadingOn,
+        handleLoadingOff,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default AuthProvider
