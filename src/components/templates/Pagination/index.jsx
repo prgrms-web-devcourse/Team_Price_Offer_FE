3dc003bfab4ca06fc74583aedbfe10aeb4afcdbb
Shinhyojeong <hyojeonge0@naver.com>
2021-12-14 21:31:43 +0900

    ✨ feat : Paging 컴포넌트 명 Pagination으로 변경

---
+++ src/components/templates/Pagination/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Pagination = ({ goodsListLength = 45 }) => {
+  return <ul>{}</ul>
+}
+
+Pagination.propTypes = {}
+
+export default Pagination
