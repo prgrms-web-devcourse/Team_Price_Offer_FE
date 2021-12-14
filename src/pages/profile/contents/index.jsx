6f8a26809f80d6419467da21dd83144ca5a39840
sonsurim <surim014@naver.com>
2021-12-14 18:16:33 +0900

    ✨ feat: profile page 예외처리

--- src/pages/profile/contents/index.jsx
+++ src/pages/profile/contents/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useRouter } from 'next/dist/client/router'
+import Spinner from '@components/templates/Spinner'
 import Like from './Like'
 import Offer from './Offer'
 import Review from './Review'
 import Sale from './Sale'
 
 const ProfileContents = ({ pageType }) => {
+  const router = useRouter()
+
+  const SpinnerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+  }
+
   switch (pageType) {
     case 'like':
       return <Like />
@@ -13,8 +22,16 @@ const ProfileContents = ({ pageType }) => {
       return <Offer />
     case 'review':
       return <Review />
-    default:
+    case 'sale':
       return <Sale />
+    default:
+      router.push('/')
+
+      return (
+        <div style={SpinnerStyle}>
+          <Spinner />
+        </div>
+      )
   }
 }
 
