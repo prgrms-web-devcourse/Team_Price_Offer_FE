23fda6539d124d10205012708c651e2f15aaadec
sonsurim <surim014@naver.com>
2021-12-14 14:41:31 +0900

    🔧 config: api base url 설정

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ const path = require('path')
 
 module.exports = {
   env: {
-    BASE_API_HOST:
-      'http://ec2-54-180-35-183.ap-northeast-2.compute.amazonaws.com:8080',
+    BASE_API_HOST: 'http://54.180.35.183:8080',
     KAKAO_API_HOST: 'https://kauth.kakao.com',
     KAKAO_API_KEY: '76823e5b8699f9ae9c2aaa0157325076',
     KAKAO_REDIRECT_URI: 'http://localhost:3000/oauth/callback/kakao',
