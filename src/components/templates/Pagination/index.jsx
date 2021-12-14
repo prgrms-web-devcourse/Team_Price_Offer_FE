e77975f90bd21fa89e9e49514cbd174e5094a13c
Shinhyojeong <hyojeonge0@naver.com>
2021-12-14 23:37:45 +0900

    ✨ feat : Pagination Component 마크업

--- src/components/templates/Pagination/index.jsx
+++ src/components/templates/Pagination/index.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { PRIMARY } from '@utils/constant'
+import {
+  PAGINATION_ARROW_LEFT,
+  PAGINATION_ARROW_RIGHT,
+} from '@utils/constant/icon'
 
-const Pagination = ({ goodsListLength = 45 }) => {
-  return <ul>{}</ul>
+const Pagination = ({
+  btnStyle,
+  className,
+  postListLength = 155,
+  postPerPage = 10,
+  paginate,
+}) => {
+  const imgStyle = {
+    cursor: 'pointer',
+  }
+
+  const pageNumbers = Array.from(
+    { length: postListLength / postPerPage },
+    (item, index) => index + 1,
+  )
+
+  console.log(PRIMARY)
+
+  return (
+    <Ul>
+      <Li>
+        <img style={imgStyle} src={PAGINATION_ARROW_LEFT} alt="이전페이지" />
+      </Li>
+      {pageNumbers.map(num => (
+        <Li key={num}>
+          <button
+            style={btnStyle}
+            className={className}
+            // onClick={() => paginate(num)}
+          >
+            {num}
+          </button>
+        </Li>
+      ))}
+      <Li>
+        <img style={imgStyle} src={PAGINATION_ARROW_RIGHT} alt="다음페이지" />
+      </Li>
+    </Ul>
+  )
 }
+
+const Ul = styled.ul`
+  list-style: none;
+`
+const Li = styled.li`
+  float: left;
+`
 
 Pagination.propTypes = {}
 
