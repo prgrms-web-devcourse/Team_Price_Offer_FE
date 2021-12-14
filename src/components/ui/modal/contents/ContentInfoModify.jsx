28b3707d98f1fbef6479dadabb683a46d4ad82b3
sonsurim <surim014@naver.com>
2021-12-15 06:24:22 +0900

    ✨ feat: 개인정보 수정 기능 구현

--- src/components/ui/modal/contents/ContentInfoModify.jsx
+++ src/components/ui/modal/contents/ContentInfoModify.jsx
@@ -1,42 +1,31 @@
 import React, { useRef } from 'react'
 import Input from '@components/templates/Input'
-import SelectBox from '@components/templates/Selectbox'
 import Button from '@components/templates/Button'
-import { CATEGORIES } from '@data/dummy/categories'
+import ImageUploader from '@components/templates/ImageUploader'
 import validate from '@utils/validation'
 import { useFormik } from 'formik'
-import { userApi, imgApi } from '@api/apis'
-import ImageUploader from '@components/templates/ImageUploader'
-import { PHOTO, NOIMG } from '@utils/constant/icon'
-
-const selectBoxdefault = {
-  code: 'selectBoxDefault',
-  name: '선택하세요',
-}
+import { imgApi } from '@api/apis'
+import { PHOTO, NOIMG } from '@utils/constant'
+import { useAuthContext } from '@hooks/useAuthContext'
 
 const ContentInfoModify = () => {
-  const noImagePath = NOIMG
-  const photoIconPath = PHOTO
+  const { state, handleModifyUserInfo } = useAuthContext()
+  const { userData } = state
 
   const formik = useFormik({
     initialValues: {
-      email: '',
-      address1: '',
-      address2: '',
+      nickname: '',
+      address: '',
     },
     validate,
     onSubmit: async values => {
       const userInfo = {
-        nickname: values.email,
-        address: `${values.address1} ${values.address2}`,
+        nickname: values.nickname,
+        address: values.address,
         profileImageUrl: profileImgRef.current.src,
       }
 
-      const res = await userApi.modifyUserInfo(userInfo)
-
-      if (Number(res.status) === 200) {
-        console.log(res)
-      }
+      await handleModifyUserInfo(userInfo)
     },
   })
 
@@ -66,54 +55,46 @@ const ContentInfoModify = () => {
                 <div className="modal-body_form-photo-wrapper">
                   <img
                     ref={profileImgRef}
-                    src={noImagePath}
+                    src={
+                      userData.profileImageUrl
+                        ? userData.profileImageUrl
+                        : NOIMG
+                    }
                     alt="avatar"
                     className="modal-body_form-avatar"
                   />
                   <div className="modal-body_form-photo">
-                    <img src={photoIconPath} alt="photo_upload" />
+                    <img src={PHOTO} alt="photo_upload" />
                   </div>
                 </div>
               </ImageUploader>
             </div>
-            <div className="modal-body_form-input email">
-              <h3>이메일</h3>
+            <div className="modal-body_form-input nickname">
+              <h3>닉네임</h3>
               <Input
                 type="text"
-                name="email"
-                placeholder="이메일"
-                value={formik.values.email}
+                name="nickname"
+                placeholder={userData.nickname}
+                value={formik.values.nickname}
                 onChange={formik.handleChange}
               />
-              {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+              {formik.errors.nickname ? (
+                <div>{formik.errors.nickname}</div>
+              ) : null}
             </div>
             <div className="modal-body_form-input address">
               <h3>주소</h3>
-              <div className="modal-body_select-wrapper">
-                <div>
-                  <SelectBox
-                    formName="address1"
-                    options={CATEGORIES}
-                    defaultOption={selectBoxdefault}
-                    value={formik.values.address1}
-                    onChange={formik.handleChange}
-                  />
-                  {formik.errors.address1 ? (
-                    <div>{formik.errors.address1}</div>
-                  ) : null}
-                </div>
-                <div>
-                  <SelectBox
-                    formName="address2"
-                    options={CATEGORIES}
-                    defaultOption={selectBoxdefault}
-                    value={formik.values.address2}
-                    onChange={formik.handleChange}
-                  />
-                  {formik.errors.address2 ? (
-                    <div>{formik.errors.address2}</div>
-                  ) : null}
-                </div>
+              <div>
+                <Input
+                  type="text"
+                  name="address"
+                  placeholder={userData.address}
+                  value={formik.values.address}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.address ? (
+                  <div>{formik.errors.address}</div>
+                ) : null}
               </div>
             </div>
           </div>
