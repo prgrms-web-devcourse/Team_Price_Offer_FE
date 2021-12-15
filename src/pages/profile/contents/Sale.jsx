7ceaa8bd3da441c879a47007239aff0b1a046e79
sonsurim <surim014@naver.com>
2021-12-16 02:48:05 +0900

    ✨ feat: 사용자 페이지 - 판매 상품 api request 구현

--- src/pages/profile/contents/Sale.jsx
+++ src/pages/profile/contents/Sale.jsx
@@ -1,21 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Divider from '@components/templates/Divider'
 import Button from '@components/templates/Button'
 import GoodsList from '@components/ui/GoodsList'
-import { GOODSLIST } from '@data/dummy/goodsList'
+import { userApi } from '@api/apis'
 
 const Sale = ({ userId }) => {
-  console.log(userId)
+  const [goodsList, setGoodsList] = useState([])
+  const [goodsListStatus, setGoodsListStatus] = useState({
+    isSelling: true,
+  })
 
-  useEffect(() => {}, [])
+  useEffect(async () => {
+    if (goodsListStatus.isSelling) {
+      const res = await userApi.getUserTradingAtricles({ memberId: userId })
+
+      if (Number(res.code) !== 200) {
+        return alert('판매중인 상품 조회 시, 문제가 발생하였습니다!')
+      }
+
+      setGoodsList(res.data.elements)
+      return
+    }
+
+    const res = await userApi.getUserCompletedArticles({ memberId: userId })
+
+    if (Number(res.code) !== 200) {
+      return alert('판매완료 상품 조회 시, 문제가 발생하였습니다!')
+    }
+
+    setGoodsList(res.data.elements)
+  }, [goodsListStatus])
 
   return (
     <div className="result-container">
-      <div className="result-title">가격 제안</div>
+      <div className="result-title">판매 상품</div>
       <div className="result-btn-box">
-        <Button className="result-btn_item selected">판매 중</Button>
-        <Button className="result-btn_item">판매 완료</Button>
+        <Button
+          onClick={() => setGoodsListStatus({ isSelling: true })}
+          className={`result-btn_item ${
+            goodsListStatus.isSelling ? 'selected' : ''
+          }`}>
+          판매 중
+        </Button>
+        <Button
+          onClick={() => setGoodsListStatus({ isSelling: false })}
+          className={`result-btn_item ${
+            !goodsListStatus.isSelling ? 'selected' : ''
+          }`}>
+          판매 완료
+        </Button>
       </div>
       <div className="result-lineup-box">
         <span className="result-lineup_item selected">최신순</span>
@@ -25,7 +59,7 @@ const Sale = ({ userId }) => {
         <span className="result-lineup_item">높은 가격순</span>
       </div>
       <div className="result-content">
-        <GoodsList goodsList={GOODSLIST} className="sale-goodList" />
+        <GoodsList goodsList={goodsList} className="sale-goodList" />
       </div>
     </div>
   )
