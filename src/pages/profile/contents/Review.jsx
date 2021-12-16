2454c5308840739824278a7998fc4fb126457f73
sonsurim <surim014@naver.com>
2021-12-17 04:37:53 +0900

    ✨ feat: 리뷰 모달 보기 기능 구현

--- src/pages/profile/contents/Review.jsx
+++ src/pages/profile/contents/Review.jsx
@@ -4,9 +4,12 @@ import Avatar from '@components/templates/Avatar'
 import Button from '@components/templates/Button'
 import Divider from '@components/templates/Divider'
 import Pagination from '@components/templates/Pagination'
+import ModalMyReview from '@components/ui/modal/ModalMyReview'
+import ModalWriteReview from '@components/ui/modal/ModalWriteReview'
 import { userApi } from '@api/apis'
 
 const Review = ({ userId }) => {
+  const [postIdOfReview, setPostIdOfReview] = useState(null)
   const [goodsList, setGoodsList] = useState({
     elements: [],
     totalElementCount: 0,
@@ -21,6 +24,8 @@ const Review = ({ userId }) => {
       size: 10,
     },
   })
+  const [visibleReviewModal, setVisibleReviewModal] = useState(false)
+  const [visibleWriteReviewModal, setVisibleWriteReviewModal] = useState(false)
 
   const handleCheckReviews = pageNum => {
     setCheckReviewOptions({
@@ -30,6 +35,17 @@ const Review = ({ userId }) => {
         page: pageNum,
       },
     })
+  }
+
+  const handleReviewModal = (postId, isWriteModal) => {
+    setPostIdOfReview(postId)
+
+    if (isWriteModal) {
+      setVisibleWriteReviewModal(true)
+      return
+    }
+
+    setVisibleReviewModal(true)
   }
 
   useEffect(async () => {
@@ -65,105 +81,125 @@ const Review = ({ userId }) => {
     .src
 
   return (
-    <div className="result-container">
-      <div className="result-title">거래 후기</div>
-      <div className="result-btn-box">
-        <Button
-          onClick={() => setGoodsListStatus({ isSellingReview: true })}
-          className={`result-btn_item ${
-            goodsListStatus.isSellingReview ? 'selected' : ''
-          }`}>
-          판매 후기
-        </Button>
-        <Button
-          onClick={() => setGoodsListStatus({ isSellingReview: false })}
-          className={`result-btn_item ${
-            !goodsListStatus.isSellingReview ? 'selected' : ''
-          }`}>
-          구매 후기
-        </Button>
-      </div>
-      <div className="result-lineup-box">
-        <span className="result-lineup_item selected">최신순</span>
-        <Divider type="vertical" />
-        <span className="result-lineup_item">낮은 가격순</span>
-        <Divider type="vertical" />
-        <span className="result-lineup_item">높은 가격순</span>
-      </div>
-      <div className="result-content">
-        <div className="review">
-          <ul className="review-list" style={ulStyle}>
-            {goodsList.elements.map(item => (
-              <>
-                <li key={item.id} className="review-item">
-                  <div className="review-profile-box">
-                    <Avatar
-                      src={item.reviewer.profileImageUrl}
-                      className="review-profile-img"
-                      style={avatarStyle}
-                    />
-                    <div className="review-profile_inform">
-                      <div className="review-reviewer">
-                        <span className="review-reviewer_name">
-                          {item.reviewer.nickname}
-                        </span>
-                        <span className="review-reviewer_level">
-                          Lv. {item.reviewer.offerLevel}
-                        </span>
+    <>
+      <div className="result-container">
+        <div className="result-title">거래 후기</div>
+        <div className="result-btn-box">
+          <Button
+            onClick={() => setGoodsListStatus({ isSellingReview: true })}
+            className={`result-btn_item ${
+              goodsListStatus.isSellingReview ? 'selected' : ''
+            }`}>
+            판매 후기
+          </Button>
+          <Button
+            onClick={() => setGoodsListStatus({ isSellingReview: false })}
+            className={`result-btn_item ${
+              !goodsListStatus.isSellingReview ? 'selected' : ''
+            }`}>
+            구매 후기
+          </Button>
+        </div>
+        <div className="result-lineup-box">
+          <span className="result-lineup_item selected">최신순</span>
+          <Divider type="vertical" />
+          <span className="result-lineup_item">낮은 가격순</span>
+          <Divider type="vertical" />
+          <span className="result-lineup_item">높은 가격순</span>
+        </div>
+        <div className="result-content">
+          <div className="review">
+            <ul className="review-list" style={ulStyle}>
+              {goodsList.elements.map(item => (
+                <>
+                  <li key={item.id} className="review-item">
+                    <div className="review-profile-box">
+                      <Avatar
+                        src={item.reviewer.profileImageUrl}
+                        className="review-profile-img"
+                        style={avatarStyle}
+                      />
+                      <div className="review-profile_inform">
+                        <div className="review-reviewer">
+                          <span className="review-reviewer_name">
+                            {item.reviewer.nickname}
+                          </span>
+                          <span className="review-reviewer_level">
+                            Lv. {item.reviewer.offerLevel}
+                          </span>
+                        </div>
+                        <div className="review-reviewer_content">
+                          {item.content}
+                        </div>
+                        <Button
+                          className="review-goods_btn-wrapper"
+                          style={{ border: '1px solid #ccc' }}>
+                          <div className="review-goods_btn">
+                            <div className="review-goods_btn_text">
+                              {goodsListStatus.isSellingReview
+                                ? '판매상품'
+                                : '구매상품'}
+                              <Divider type="vertical" />
+                              {item.article.title}
+                            </div>
+                            <div className="review-goods_btn_icn">
+                              <img
+                                src={arrowImgPath}
+                                alt="arrow"
+                                className="review-goods_icon_arrow"
+                              />
+                            </div>
+                          </div>
+                        </Button>
                       </div>
-                      <div className="review-reviewer_content">
-                        {item.content}
+                    </div>
+                    <div className="review-side-box">
+                      <div style={{ width: '100%', textAlign: 'right' }}>
+                        {item.time}
                       </div>
                       <Button
-                        className="review-goods_btn-wrapper"
-                        style={{ border: '1px solid #ccc' }}>
-                        <div className="review-goods_btn">
-                          <div className="review-goods_btn_text">
-                            {goodsListStatus.isSellingReview
-                              ? '판매상품'
-                              : '구매상품'}
-                            <Divider type="vertical" />
-                            {item.article.title}
-                          </div>
-                          <div className="review-goods_btn_icn">
-                            <img
-                              src={arrowImgPath}
-                              alt="arrow"
-                              className="review-goods_icon_arrow"
-                            />
-                          </div>
-                        </div>
+                        onClick={() =>
+                          handleReviewModal(
+                            item.article.id,
+                            item.isWritingAvailableFromCurrentMember,
+                          )
+                        }
+                        style={reviewBtnStyle}
+                        className={`review-review_btn ${
+                          item.isWritingAvailableFromCurrentMember && 'leave'
+                        }`}>
+                        {item.isWritingAvailableFromCurrentMember
+                          ? '후기 남기기'
+                          : '후기 보기'}
                       </Button>
                     </div>
-                  </div>
-                  <div className="review-side-box">
-                    <div style={{ width: '100%', textAlign: 'right' }}>
-                      {item.time}
-                    </div>
-                    <Button
-                      style={reviewBtnStyle}
-                      className={`review-review_btn ${
-                        item.isWritingAvailableFromCurrentMember && 'leave'
-                      }`}>
-                      {!item.isWritingAvailableFromCurrentMember
-                        ? '후기 보기'
-                        : '후기 남기기'}
-                    </Button>
-                  </div>
-                </li>
-                <hr />
-              </>
-            ))}
-          </ul>
+                  </li>
+                  <hr />
+                </>
+              ))}
+            </ul>
+          </div>
         </div>
+        <Pagination
+          size={checkReviewOptions.params.size}
+          postListLength={goodsList.totalElementCount}
+          paginate={handleCheckReviews}
+          setStartPage={handleCheckReviews}
+        />
       </div>
-      <Pagination
-        size={checkReviewOptions.params.size}
-        postListLength={goodsList.totalElementCount}
-        paginate={handleCheckReviews}
-        setStartPage={handleCheckReviews}
-      />
-    </div>
+      <ModalMyReview
+        postId={postIdOfReview}
+        visible={visibleReviewModal}
+        onClose={() => setVisibleReviewModal(false)}>
+        리뷰 보기 모달
+      </ModalMyReview>
+      <ModalWriteReview
+        postId={postIdOfReview}
+        visible={visibleWriteReviewModal}
+        onClose={() => setVisibleWriteReviewModal(false)}>
+        리뷰 쓰기 모달
+      </ModalWriteReview>
+    </>
   )
 }
 
