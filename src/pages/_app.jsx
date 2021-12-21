a2b2ee23a4497e8ccf08608a2923fec141416eae
sonsurim <surim014@naver.com>
2021-12-22 08:04:31 +0900

    ✨ feat: next js router spinner 구현

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,23 +4,27 @@ import LoadingProvider from '@contexts/LoadingProvider'
 import AuthProvider from '@contexts/AuthProvider'
 import AxiosProvider from '@contexts/AxiosProvider'
 import { Provider as BusProvider } from 'react-bus'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LoadingProvider>
-      <AxiosProvider>
-        <AuthProvider>
-          <BusProvider>
-            <div className="container">
-              <div className="wrapper">
-                <Header />
-                <Component {...pageProps} />
+    <>
+      <NextNProgress color="#F74F2A" />
+      <LoadingProvider>
+        <AxiosProvider>
+          <AuthProvider>
+            <BusProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Header />
+                  <Component {...pageProps} />
+                </div>
               </div>
-            </div>
-          </BusProvider>
-        </AuthProvider>
-      </AxiosProvider>
-    </LoadingProvider>
+            </BusProvider>
+          </AuthProvider>
+        </AxiosProvider>
+      </LoadingProvider>
+    </>
   )
 }
 
